a1ea4cd54216da3cc0d621b1350480c29836fbe5
NirodhaXN <153802716+NirodhaXN@users.noreply.github.com>
2023-12-14 17:08:50 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(steps)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+    steps += 1
+})
+let steps = 0
+steps = 0
